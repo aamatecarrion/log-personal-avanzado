1b2d274ef2852c308dd4049ac5a2f9cb15e3eb96
aamatecarrion <aamatecarrion@gmail.com>
2025-05-21 00:02:45 +0200

    fix: correct event handler syntax in MapShow component

--- resources/js/components/map-show.tsx
+++ resources/js/components/map-show.tsx
@@ -35,7 +35,7 @@ export function MapShow({record}: { record: Record }) {
                 },
               }
               );
-            }
+            },
           }}>
           </Marker>
         )}
