50c607521a6b21409e2fe75a8c50df7149e919fa
aamatecarrion <aamatecarrion@gmail.com>
2025-05-26 07:46:27 +0200

    Update API endpoint in ProcessImage job to use the correct IP address

--- app/Jobs/ProcessImage.php
+++ app/Jobs/ProcessImage.php
@@ -34,7 +34,7 @@ class ProcessImage implements ShouldQueue
             // Convertir a base64
             $imageData = base64_encode(Storage::disk('private')->get($this->image->image_path));
 
-            $response = Http::timeout(240)->post('http://localhost:11434/api/generate', [
+            $response = Http::timeout(240)->post('http://192.168.1.20:11434/api/generate', [
                 'model' => env('OLLAMA_MODEL'),
                 'prompt' => 'genera una descripción para esta imagen, (no digas cosas que formen parte de una conversación cómo: aquí hay una descripción, por supuesto o Claro! te describiré la imagen )',
                 'images' => [$imageData],
@@ -58,7 +58,7 @@ class ProcessImage implements ShouldQueue
                 'generated_description' => $generated_description
             ]);
 
-            $response = Http::timeout(240)->post('http://localhost:11434/api/generate', [
+            $response = Http::timeout(240)->post('http://192.168.1.20:11434/api/generate', [
                 'model' => env('OLLAMA_MODEL'),
                 'prompt' => 'describe esta imagen en menos de 10 palabras (no digas cosas que formen parte de una conversación cómo: aquí hay una descripción, por supuesto o Claro! te describiré la imagen )',
                 'images' => [$imageData],
