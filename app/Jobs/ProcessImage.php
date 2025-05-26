385da14739a12f81936cffdde89c9950c2b3a485
aamatecarrion <aamatecarrion@gmail.com>
2025-05-26 22:02:56 +0200

    Update API endpoint in ProcessImage job to use localhost instead of local IP address

--- app/Jobs/ProcessImage.php
+++ app/Jobs/ProcessImage.php
@@ -40,7 +40,7 @@ class ProcessImage implements ShouldQueue
  */
             $imageData = base64_encode($rawImage);
 
-            $response = Http::timeout(240)->post('http://192.168.1.20:11434/api/generate', [
+            $response = Http::timeout(240)->post('http://localhost:11434/api/generate', [
                 'model' => env('OLLAMA_MODEL'),
                 'prompt' => 'genera una descripción para esta imagen, (no digas cosas que formen parte de una conversación cómo: aquí hay una descripción, por supuesto o Claro! te describiré la imagen )',
                 'images' => [$imageData],
@@ -64,7 +64,7 @@ class ProcessImage implements ShouldQueue
                 'generated_description' => $generated_description
             ]);
 
-            $response = Http::timeout(240)->post('http://192.168.1.20:11434/api/generate', [
+            $response = Http::timeout(240)->post('http://localhost:11434/api/generate', [
                 'model' => env('OLLAMA_MODEL'),
                 'prompt' => 'describe esta imagen en menos de 10 palabras (no digas cosas que formen parte de una conversación cómo: aquí hay una descripción, por supuesto o Claro! te describiré la imagen )',
                 'images' => [$imageData],
