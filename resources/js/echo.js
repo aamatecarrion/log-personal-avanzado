c36633664e399d127e7414fb4648458955ed74b7
aamatecarrion <aamatecarrion@gmail.com>
2025-07-19 15:35:06 +0200

    come on

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,5 +1,5 @@
 import Pusher from 'pusher-js';
-window.Pusher = Pusher; // 👈 esto es imprescindible para Reverb
+window.Pusher = Pusher;
 
 import Echo from 'laravel-echo';
 
@@ -7,10 +7,10 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: 443,
-    wssPort: 443,
+    wsPort: 30600,         // el puerto expuesto al exterior
+    wssPort: 30600,
     forceTLS: true,
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: ['ws','wss'],
     authEndpoint: '/broadcasting/auth',
-    wsPath: '/app-reverb',
+    wsPath: '/app-reverb', // misma ruta que en Apache
 });
