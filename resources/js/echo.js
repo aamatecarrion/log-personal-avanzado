8a0bcf425c849fbffed53a4b5085c5c8b200e1c8
aamatecarrion <aamatecarrion@gmail.com>
2025-07-19 14:50:55 +0200

    fix: Update Echo configuration to ensure proper Reverb setup

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,20 +1,14 @@
-import Pusher from 'pusher-js';
-window.Pusher = Pusher;
-
 import Echo from 'laravel-echo';
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    client: new Pusher(import.meta.env.VITE_REVERB_APP_KEY, {
-        wsHost: import.meta.env.VITE_REVERB_HOST,
-        wsPort: 443,
-        wssPort: 443,
-        forceTLS: true,
-        enabledTransports: ['ws', 'wss'],
-        disableStats: true,
-        authEndpoint: '/broadcasting/auth',
-        // 🔥 ESTA ES LA CLAVE
-        // añade el path manualmente en el `wsPath`
-        wsPath: '/app-reverb',
-    }),
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
+    enabledTransports: ['ws', 'wss'],
+    disableStats: true,
+    authEndpoint: '/broadcasting/auth',
+    wsPath: '/app-reverb',
 });
